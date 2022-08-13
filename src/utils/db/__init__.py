5e9cca878e9ddf312439540ac763dd6e9aba9997
airo.pi_ <47398145+AiroPi@users.noreply.github.com>
2022-08-13 18:54:13 +0200

    fix: db use new logger structure

--- src/utils/db/__init__.py
+++ src/utils/db/__init__.py
@@ -1,26 +1,25 @@
 from __future__ import annotations
 
+import logging
 import sys
 from os import environ
 from typing import TYPE_CHECKING
 
 from sqlalchemy.ext.asyncio import create_async_engine
 
-from ..logger import INFO, create_logger
 from .tables import guilds as guilds
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncEngine
 
-LOG_LEVEL = int(tmp) if (tmp := environ.get("LOG_LEVEL")) and tmp.isdigit() else INFO
-db_logger = create_logger(__name__, level=INFO)
+logger = logging.getLogger(__name__)
 
 try:
     POSTGRES_USER = environ["POSTGRES_USER"]
     POSTGRES_PASSWORD = environ["POSTGRES_PASSWORD"]
     POSTGRES_DB = environ["POSTGRES_DB"]
 except KeyError as e:
-    db_logger.critical(f"Missing environment variable {e}.")
+    logger.critical(f"Missing environment variable {e}.")
     sys.exit(1)
 
 db: AsyncEngine = create_async_engine(
