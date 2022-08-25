98f55f35ac5491d92ac818c4746ef3dab6bff3fe
airo.pi_ <47398145+AiroPi@users.noreply.github.com>
2022-08-26 00:46:27 +0200

    feat: add some emojis to constants

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,3 +1,27 @@
 from __future__ import annotations
 
-# no constants yet
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from utils._types import Snowflake
+
+
+class Emoji(str):
+    def __new__(cls, id: Snowflake) -> Emoji:
+        return super().__new__(cls, f"<:e:{id}>")
+
+    def __init__(self, id: Snowflake) -> None:
+        self._id = id
+        super().__init__()
+
+    @property
+    def id(self):
+        return self._id
+
+
+class Emojis:
+    beta_1 = Emoji(1012449366449078384)
+    beta_2 = Emoji(1012449349424390265)
+
+
+print(Emojis.beta_1)
