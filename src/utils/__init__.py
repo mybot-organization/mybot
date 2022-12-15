7763bf407b27274a91552e9d39e5ea14979907f0
airo.pi_ <47398145+AiroPi@users.noreply.github.com>
2022-12-16 00:08:30 +0100

    refactor: response type allow unpacking + attributes

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,14 +1,37 @@
 import logging
+from dataclasses import dataclass
 from enum import Enum, auto
-from typing import TypedDict
+from typing import Any, Iterator, Literal, Mapping, TypedDict, overload
 
 import discord
+from discord import Embed
 
 logger = logging.getLogger(__name__)
 
 
 class Response(TypedDict):
-    embed: discord.Embed
+    embed: Embed | None
+    content: str | None
+
+
+@dataclass()
+class _Response(Mapping[str, Embed]):
+    embed: Embed | None = None
+    content: str | None = None
+
+    def __getitem__(self, key: str) -> Any:
+        return self.__dict__[key]
+
+    def __iter__(self) -> Iterator[str]:
+        return iter(self.__dict__)
+
+    def __len__(self) -> int:
+        return self.__dict__.__len__()
+
+
+class _ResponseEmbed(_Response):
+    embed: Embed
+    content: str | None = None
 
 
 class ResponseType(Enum):
@@ -33,7 +56,17 @@ _embed_author_icon_urls = {
 }
 
 
-def response_constructor(response_type: ResponseType, message: str, embedded: bool = True) -> Response:
+@overload
+def response_constructor(response_type: ResponseType, message: str, embedded: Literal[True] = ...) -> _ResponseEmbed:
+    ...
+
+
+@overload
+def response_constructor(response_type: ResponseType, message: str, embedded: Literal[False] = ...) -> _Response:
+    ...
+
+
+def response_constructor(response_type: ResponseType, message: str, embedded: bool = True) -> _Response:
     embed = discord.Embed(
         color=_embed_colors[response_type],
     )
@@ -43,4 +76,4 @@ def response_constructor(response_type: ResponseType, message: str, embedded: bo
 
     embed.set_author(name=message, icon_url=_embed_author_icon_urls[response_type])
 
-    return {"embed": embed}
+    return _Response(embed=embed)
