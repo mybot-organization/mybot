870b7a5eefb35999a05840223ef991039f05b7cf
airo.pi_ <47398145+AiroPi@users.noreply.github.com>
2023-02-22 16:19:19 +0100

    feat: add Emojis to core direct exports

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -8,6 +8,7 @@ from discord import Embed
 
 from ._config import config as config
 from .cog_property import cog_property as cog_property
+from .constants import Emojis as Emojis
 from .misc_command import misc_command as misc_command
 from .special_cog import SpecialCog as SpecialCog, SpecialGroupCog as SpecialGroupCog
 from .temporary_cache import TemporaryCache as TemporaryCache
