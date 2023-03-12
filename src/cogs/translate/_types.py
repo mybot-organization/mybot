ffaf2604b6ae0db1cdde1149ce9636cb07c82c40
airo.pi_ <47398145+AiroPi@users.noreply.github.com>
2023-03-12 02:58:10 +0100

    feat: refactor translate protocol, etc.

--- src/cogs/translate/_types.py
+++ src/cogs/translate/_types.py
@@ -1,30 +1,65 @@
 from __future__ import annotations
 
-from typing import Any, Protocol, Self, Sequence, TypeVar
+from typing import TYPE_CHECKING, Any, Protocol, Self, Sequence, TypeVar, overload, runtime_checkable
 
 from discord import Embed, Locale
+from discord.utils import MISSING
 
-_LP = TypeVar("_LP", bound="LanguageProtocol")
+if TYPE_CHECKING:
+    LP = TypeVar("LP", bound="LanguageProtocol")
+
+T = TypeVar("T")
 
 # Protocol for Language and Translations methods are maybe not the best idea
 # Will probably change this in the future
 
 
+@runtime_checkable
 class LanguageProtocol(Protocol):
     name: str
     code: str
     locale: Locale | None
 
+    @overload
     @classmethod
-    def from_emote(cls, emote: str) -> LanguageProtocol | None:
+    def from_emote(cls, emote: str) -> Self:
+        ...
+
+    @overload
+    @classmethod
+    def from_emote(cls, emote: str, default: T) -> Self | T:
         ...
 
     @classmethod
-    def from_discord_locale(cls, locale: Locale) -> LanguageProtocol | None:
+    def from_emote(cls, emote: str, default: T = MISSING) -> Self | T:
+        ...
+
+    @overload
+    @classmethod
+    def from_discord_locale(cls, locale: Locale) -> Self:
+        ...
+
+    @overload
+    @classmethod
+    def from_discord_locale(cls, locale: Locale, default: T) -> Self | T:
         ...
 
     @classmethod
-    def from_code(cls, code: str) -> Self | None:
+    def from_discord_locale(cls, locale: Locale, default: T = MISSING) -> Self | T:
+        ...
+
+    @overload
+    @classmethod
+    def from_code(cls, code: str) -> Self:
+        ...
+
+    @overload
+    @classmethod
+    def from_code(cls, code: str, default: T) -> Self | T:
+        ...
+
+    @classmethod
+    def from_code(cls, code: str, default: T = MISSING) -> Self | T:
         ...
 
     @classmethod
@@ -33,12 +68,12 @@ class LanguageProtocol(Protocol):
 
 
 class TranslatorFunction(Protocol):
-    async def __call__(self, text: str, to: _LP, from_: _LP | None = None) -> str:
+    async def __call__(self, text: str, to: LP, from_: LP | None = None) -> str:
         ...
 
 
 class BatchTranslatorFunction(Protocol):
-    async def __call__(self, texts: Sequence[str], to: _LP, from_: _LP | None = None) -> Sequence[str]:
+    async def __call__(self, texts: Sequence[str], to: LP, from_: LP | None = None) -> Sequence[str]:
         ...
 
 
