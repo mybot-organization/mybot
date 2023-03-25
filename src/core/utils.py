c2a86253a663f72d869e3d75919b1d681252946d
airo.pi_ <47398145+AiroPi@users.noreply.github.com>
2023-03-26 00:01:15 +0100

    feat: add utils functions that works like any & all but in async

--- src/core/utils.py
+++ src/core/utils.py
@@ -1,5 +1,5 @@
 from enum import Enum, auto
-from typing import Iterator, Sequence, TypeVar
+from typing import AsyncIterable, Iterator, Sequence, TypeVar
 
 T = TypeVar("T")
 
@@ -16,3 +16,17 @@ def chunker(seq: Sequence[T], size: int) -> Iterator[Sequence[T]]:
 def splitter(seq: Sequence[T], number: int) -> Iterator[Sequence[T]]:
     size = (len(seq) + number - 1) // number
     return chunker(seq, size)
+
+
+async def async_any(iterable: AsyncIterable[bool]) -> bool:
+    async for element in iterable:
+        if element:
+            return True
+    return False
+
+
+async def async_all(iterable: AsyncIterable[bool]) -> bool:
+    async for element in iterable:
+        if not element:
+            return False
+    return True
