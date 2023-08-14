e93038fe02e3a8ac18c1ff18fabd726b89220586
airo.pi_ <47398145+AiroPi@users.noreply.github.com>
2023-08-14 17:49:57 +0200

    adapt checkers for the new structure

--- src/cogs/restore.py
+++ src/cogs/restore.py
@@ -4,7 +4,7 @@ import logging
 from typing import TYPE_CHECKING
 
 from core import SpecialCog, misc_command
-from core.checkers import is_activated, is_user_authorized, misc_check, misc_cmd_bot_required_permissions
+from core.checkers.misc import bot_required_permissions, is_activated, is_user_authorized, misc_check
 
 if TYPE_CHECKING:
     from discord import Message
@@ -17,7 +17,7 @@ logger = logging.getLogger(__name__)
 
 class Restore(SpecialCog["MyBot"]):
     @misc_command("restore", description="Send a message back in chat if a link is send.", extras={"soon": True})
-    @misc_cmd_bot_required_permissions(manage_webhooks=True)
+    @bot_required_permissions(manage_webhooks=True)
     @misc_check(is_activated)
     @misc_check(is_user_authorized)
     async def on_message(self, message: Message) -> None:
